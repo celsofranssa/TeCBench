ff3f19590263427bed4c78078d93f627caa77796
Celso <c.el@live.com>
2022-03-17 18:39:30 -0300

    feat: add SeparabilityHelper.py helper.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from omegaconf import OmegaConf
 from source.helper.EvalHelper import EvalHelper
 from source.helper.FitHelper import FitHelper
 from source.helper.PredictHelper import PredictHelper
+from source.helper.SeparabilityHelper import SeparabilityHelper
 from source.helper.TSNEHelper import TSNEHelper
 
 
@@ -30,20 +31,31 @@ def tsne(params):
     tsne_helper.perform_tsne()
 
 
+def separability(params):
+    separability_helper = SeparabilityHelper(params)
+    separability_helper.perform_eval()
+
 @hydra.main(config_path="settings/", config_name="settings.yaml")
 def perform_tasks(params):
     os.chdir(hydra.utils.get_original_cwd())
     OmegaConf.resolve(params)
     if "fit" in params.tasks:
         fit(params)
+
     if "predict" in params.tasks:
         predict(params)
+
     if "eval" in params.tasks:
         eval(params)
+
     if "z-shot-cls" in params.tasks:
         z_shot_cls(params)
+
     if "tsne" in params.tasks:
         tsne(params)
+
+    if "separability" in params.tasks:
+        separability(params)
 
 
 if __name__ == '__main__':
