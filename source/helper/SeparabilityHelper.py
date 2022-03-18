8a8592fe7b81395575412042834116912cb06610
Fabiano Belem <ufmg.fbelem@pythonanalytics-gsi-des01.mpmg.mp.br>
2022-03-18 16:15:06 -0300

    Fix separability helper

--- source/helper/SeparabilityHelper.py
+++ source/helper/SeparabilityHelper.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import silhouette_score
-import tqdm
+from tqdm import tqdm
 
 class SeparabilityHelper:
     def __init__(self, params):
