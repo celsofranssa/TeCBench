3f986755a4e081de765c19fb13b7c5f2ec9a91b2
Felipe Viegas <feliperviegas@gmail.com>
2022-03-18 17:01:45 -0300

    Update helper.

--- source/helper/TSNEHelper.py
+++ source/helper/TSNEHelper.py
@@ -8,7 +8,7 @@ import torch
 from tqdm import tqdm
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
+from openTSNE import TSNE
 
 class TSNEHelper:
     def __init__(self, params):
@@ -27,6 +27,10 @@ class TSNEHelper:
         predictions_paths = sorted(
             Path(f"{self.params.prediction.dir}fold_{fold_id}/").glob("*.prd")
         )
+
+        train_ids = self._load_ids(
+            f"{self.params.data.dir}fold_{fold_id}/train.pkl"
+        )
         
         test_ids = self._load_ids(
             f"{self.params.data.dir}fold_{fold_id}/test.pkl"
@@ -35,6 +39,11 @@ class TSNEHelper:
         if test_ids is None:
             raise Exception("No test ids")
         
+        trainings = []
+        for path in tqdm(predictions_paths, desc="Loading predictions"):
+            trainings.extend(  # only generates tsne over test split
+                filter(lambda prediction: prediction["idx"] in train_ids, torch.load(path))
+            )
 
         predictions = []
         for path in tqdm(predictions_paths, desc="Loading predictions"):
@@ -42,35 +51,46 @@ class TSNEHelper:
                 filter(lambda prediction: prediction["idx"] in test_ids, torch.load(path))
             )
 
-        return predictions
+        return trainings, predictions
 
-    def tsne(self, test_rpr):
-        return TSNE(
-            n_components=2,
-            learning_rate=200,
-            init = 'random'
-        ).fit_transform(test_rpr)
+    def tsne(self, train_rpr, test_rpr):
+        tsne_obj = TSNE(
+                        perplexity=30,
+                        metric="euclidean",
+                        n_jobs=8,
+                        random_state=42,
+                        verbose=True,
+                    ).fit(train_rpr)
+        return tsne_obj.transform(test_rprs)
 
 
     def perform_tsne(self):
 
         for fold_id in self.params.data.folds:
 
-            predictions = self.load_predictions(fold_id=fold_id)
+            trainings, predictions = self.load_predictions(fold_id=fold_id)
+
+            train_rprs = []
+            for training in trainings:
+                train_rprs.append(training["rpr"])
 
             test_rprs = []
+            test_labels = []
             for prediction in predictions:
                 test_rprs.append(prediction["rpr"])
-            
+                test_labels.append(prediction["cls"])
+
+            train_rprs = np.asarray(train_rprs, dtype=np.float64)
             test_rprs = np.asarray(test_rprs, dtype=np.float64)
 
-            tsne = self.tsne(test_rprs)
+            tsne = self.tsne(train_rprs, test_rprs)
 
             sns.scatterplot(
                 tsne[:, 0],
                 tsne[:, 1],
-                # hue="cls" insert class
+                hue=test_labels
             )
+            
             Path(self.params.tsne.dir).mkdir(parents=True, exist_ok=True)
             plt.savefig(
                 f"{self.params.tsne.dir}{fold_id}.pdf",
