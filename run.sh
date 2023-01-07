f807db0c0ae7bccfa395d8992cbd18b7d134adeb
Celso <c.el@live.com>
2023-01-07 12:22:48 -0300

    feat: large datasets.

--- run.sh
+++ run.sh
@@ -5,53 +5,22 @@ export PYTHONPATH=$PATHONPATH:~/projects/TeCBench/
 python main.py \
   tasks=[fit,predict,eval] \
   model=CNN \
-  data=WEBKB
+  data=BOOKS
 
 python main.py \
   tasks=[fit,predict,eval] \
   model=CNN \
-  data=ACM
+  data=DBLP
 
 python main.py \
   tasks=[fit,predict,eval] \
   model=CNN \
-  data=AISOPOS
+  data=AGNEWS
 
 python main.py \
   tasks=[fit,predict,eval] \
   model=CNN \
-  data=PANGMOVIE
-
-python main.py \
-  tasks=[fit,predict,eval] \
-  model=CNN \
-  data=REUTERS
-
-python main.py \
-  tasks=[fit,predict,eval] \
-  model=CNN \
-  data=SST2
-
-python main.py \
-  tasks=[fit,predict,eval] \
-  model=CNN \
-  data=VADERMOVIE
-
-python main.py \
-  tasks=[fit,predict,eval] \
-  model=CNN \
-  data=VADERNYT
-
-python main.py \
-  tasks=[fit,predict,eval] \
-  model=CNN \
-  data=WEBKB
-
-python main.py \
-  tasks=[fit,predict,eval] \
-  model=CNN \
-  data=YELP2L
-
+  data=SOGOU
 
 
 
