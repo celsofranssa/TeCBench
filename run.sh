a6667cf87ac2cd2b40f9f5d43443b13eca749afc
Celso <c.el@live.com>
2023-01-07 12:35:20 -0300

    feat: large datasets.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # activate venv and set Python path
-source ~/projects/venvs/TeCBench/bin/activate
-export PYTHONPATH=$PATHONPATH:~/projects/TeCBench/
+source /projects/venvs/TeCBench/bin/activate
+export PYTHONPATH=$PATHONPATH:/projects/TeCBench/
 
 python main.py \
   tasks=[fit,predict,eval] \
