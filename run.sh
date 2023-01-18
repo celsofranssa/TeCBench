cd4323f372588887dc9e2e63284feabc444d1a02
Celso <c.el@live.com>
2023-01-18 13:18:59 -0300

    feat: add DIARIOS dataset.

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ export PYTHONPATH=$PATHONPATH:~/projects/TeCBench/
 
 python main.py \
   tasks=[fit,predict,eval] \
-  trainer.precision=16 \
-  model=LaBSE \
-  data=DIARIOS
+  trainer.precision=32 \
+  model=BERTimbau \
+  data=DIARIOS \
+  data.folds=[0]
